86a04d1005768835c7df8a0840dc7c7a28f1f9c1
Heeyoun Lim <heeyn.lim@gmail.com>
2024-08-05 01:28:09 +0900

    feature: 생산관리 작업장 서비스 테스트
    - 생산관리 Model, Repository, Service 구조 추가
    - Workcenter 한정 Repository, Service, ServiceImplTest

---
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/production/model/basic_information/OutsourcingCost.java
@@ -0,0 +1,4 @@
+package com.megazone.ERPSystem_phase2_Backend.production.model.basic_information;
+
+public class OutsourcingCost {
+}
---
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/production/model/material_requirements_planning/MaterialInputStatus.java
@@ -0,0 +1,4 @@
+package com.megazone.ERPSystem_phase2_Backend.production.model.material_requirements_planning;
+
+public class MaterialInputStatus {
+}
