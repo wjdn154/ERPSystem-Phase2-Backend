fbdb5e3a6655822e3be0d5209a183d1d0b7935db
Minseong <ckacl2512@naver.com>
2024-08-06 18:15:46 +0900

    feature: 일부 엔티티 추가

--- src/main/java/com/megazone/ERPSystem_phase2_Backend/hr/model/hr_management/Transfer.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/hr/model/hr_management/Transfer.java
@@ -1,4 +1,29 @@
-package com.megazone.ERPSystem_phase2_Backend.hr.model.basic_information_management;
+package com.megazone.ERPSystem_phase2_Backend.hr.model.hr_management;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+// 발령 기록 저장 테이블
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Transfer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Date transferDate; // 발령 날짜
+
+    @Column(nullable = false)
+    private String transferType; // 발령 유형 ( promotion : 승진, demotion, lateral )
+
+    @Column(nullable = false)
+    private String reason; // 발령 사유
 }
