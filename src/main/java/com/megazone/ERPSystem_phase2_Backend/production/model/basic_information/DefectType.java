c622dbb4ad52869c9fdf80e3c848edf86d2fc0c3
ready-oun <heeyn.lim@gmail.com>
2024-08-07 09:32:11 +0900

    feature: 기초정보등록 일부 수정

--- src/main/java/com/megazone/ERPSystem_phase2_Backend/production/model/basic_information/DefectType.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/production/model/basic_information/DefectType.java
@@ -1,4 +1,14 @@
 package com.megazone.ERPSystem_phase2_Backend.production.model.basic_information;
 
 public class DefectType {
+
+    // Long Id
+
+    // 불량유형코드
+
+    // 불량군 enum ?
+
+    // 불량유형명
+
+    // 사용여부
 }
--- src/main/java/com/megazone/ERPSystem_phase2_Backend/production/model/basic_information/OutsourcingCost.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/production/model/basic_information/OutsourcingCost.java
@@ -1,4 +1,24 @@
 package com.megazone.ERPSystem_phase2_Backend.production.model.basic_information;
 
 public class OutsourcingCost {
+
+    // Long Id
+
+    // 품목 ( 품목군(폼목군코드, 품목군명) , 품번, 품명, 규격, 단위 )
+
+    // 표준원가
+
+    // 실제원가
+
+    // 외주단가
+
+    // 외주처 ( 거래처 )
+
+    // Double 단가적용비율
+
+    // 시작일
+
+    // 종료일
+
+    // 사용 여부
 }
