76cfe69cfac55b8687377179ff29fcf318e706f1
chanminkim-K <snoopy0813@naver.com>
2024-08-08 17:21:54 +0900

    feature: 오류 수정
    - 오류 수정

--- src/main/java/com/megazone/ERPSystem_phase2_Backend/logistics/repository/product_registration/ProductGroup/ProductGroupRepository.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/logistics/repository/product_registration/ProductGroup/ProductGroupRepository.java
@@ -1,12 +1,13 @@
-package com.megazone.ERPSystem_phase2_Backend.logistics.repository.product_registration.ProductGroup;
+//package com.megazone.ERPSystem_phase2_Backend.logistics.repository.product_registration.ProductGroup;
+//
+//import com.megazone.ERPSystem_phase2_Backend.logistics.model.product_registration.Product;
+//import com.megazone.ERPSystem_phase2_Backend.logistics.model.product_registration.ProductGroup;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.Optional;
+//
+//public interface ProductGroupRepository extends JpaRepository<ProductGroup, Long>, ProductGroupCustom {
+//    Optional<Product> findByName(String name);
+//    Optional<Product> findByCode(String code);
+//}
 
-import com.megazone.ERPSystem_phase2_Backend.logistics.model.product_registration.Product;
-import com.megazone.ERPSystem_phase2_Backend.logistics.model.product_registration.ProductGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface ProductGroupRepository extends JpaRepository<ProductGroup, Long>, ProductGroupCustom {
-    Optional<ProductGroup> findByName(String name);
-    Optional<ProductGroup> findByCode(String code);
-}
