80f08b1f9bbf330dd0f85fdd05b49bfe2af88ced
jh <chlwlgur0407@naver.com>
2024-08-08 13:36:53 +0900

    fix: 창고관리 오류 해결
    - 계층그룹 오류 해결
    - 주소 엔티티 이름 추가

--- src/main/java/com/megazone/ERPSystem_phase2_Backend/logistics/repository/basic_information_management/HierarchyGroup/HierarchyGroupRepository.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/logistics/repository/basic_information_management/HierarchyGroup/HierarchyGroupRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface HierarchyGroupRepository extends JpaRepository<HierarchyGroup, Long>, HierarchyGroupRepositoryCustom {
-    Optional<HierarchyGroup> findByName(String name);
-
-    Optional<HierarchyGroup> findByCode(String code);
+//    Optional<HierarchyGroup> findByName(String name);
+//
+//    Optional<HierarchyGroup> findByCode(String code);
 }
--- src/main/java/com/megazone/ERPSystem_phase2_Backend/production/model/outsourcing/OutsourcingPerformance.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/production/model/outsourcing/OutsourcingPerformance.java
@@ -1,49 +1,49 @@
-package com.megazone.ERPSystem_phase2_Backend.production.model.outsourcing;
-
-//외주 실적 관리
-
-import com.megazone.ERPSystem_phase2_Backend.production.model.basic_information.Process;
-import com.megazone.ERPSystem_phase2_Backend.production.model.basic_information.Workcenter;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.Date;
-
-@Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class OutsourcingPerformance {
-
-    @Id @GeneratedValue
-    private Long id;
-
-    @Column
-    private String materialStatus;
-    //자재 사용 유무
-    @Column
-    private Date date;                   //실적일
-
-    @JoinColumn(name = "process_id")
-    @Column
-    private Process process;             //공정
-
-    @JoinColumn(name = "workcenter_id")
-    @Column
-    private Workcenter workcenter;       //작업장
-
-    @Column
-    private String sortation;            //구분
-
-    @Column
-    private String performanceClassification;       //실적 구분 (적합/비적합)
-
-    @Column
-    private Long performanceQuantity;               //실적 수량
-
-    @Column
-    private String prosecutor;                      //검사(검사/무검사)
-
-}
+//package com.megazone.ERPSystem_phase2_Backend.production.model.outsourcing;
+//
+////외주 실적 관리
+//
+////import com.megazone.ERPSystem_phase2_Backend.production.model.basic_information.Process;
+//import com.megazone.ERPSystem_phase2_Backend.production.model.basic_information.Workcenter;
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.Date;
+//
+//@Entity
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class OutsourcingPerformance {
+//
+//    @Id @GeneratedValue
+//    private Long id;
+//
+//    @Column
+//    private String materialStatus;
+//    //자재 사용 유무
+//    @Column
+//    private Date date;                   //실적일
+//
+//    @JoinColumn(name = "process_id")
+//    @Column
+//    private Process process;             //공정
+//
+//    @JoinColumn(name = "workcenter_id")
+//    @Column
+//    private Workcenter workcenter;       //작업장
+//
+//    @Column
+//    private String sortation;            //구분
+//
+//    @Column
+//    private String performanceClassification;       //실적 구분 (적합/비적합)
+//
+//    @Column
+//    private Long performanceQuantity;               //실적 수량
+//
+//    @Column
+//    private String prosecutor;                      //검사(검사/무검사)
+//
+//}
