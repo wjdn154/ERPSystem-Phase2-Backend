2fc48c2cfe138e1e0b80d67a80729544ad32904a
ready-oun <heeyn.lim@gmail.com>
2024-08-08 14:44:42 +0900

    fix: hr, logistics 에러 해결
    - HR leave 주석처리
    - LOGISTICS order, currency, quotation 주석처리

--- src/main/java/com/megazone/ERPSystem_phase2_Backend/logistics/model/sales_management/quotation_registration/Currency.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/logistics/model/sales_management/quotation_registration/Currency.java
@@ -1,33 +1,33 @@
-package com.megazone.ERPSystem_phase2_Backend.logistics.model.sales_management.quotation_registration;
-
-import jakarta.persistence.*;
-import lombok.*;
-
-/**
- * 통화 엔티티
- * 통화 등록 시 사용되는 엔티티
- */
-@Entity
-@Getter
-@Builder
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-public class Currency {
-
-    // 고유 식별자
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    // 통화 코드
-    @Column(nullable = false)
-    private String code;
-
-    // 통화명
-    @Column(nullable = false)
-    private String name;
-
-    // 환율
-    @Column(nullable = false)
-    private Double exchangeRate;
-}
+//package com.megazone.ERPSystem_phase2_Backend.logistics.model.sales_management.quotation_registration;
+//
+//import jakarta.persistence.*;
+//import lombok.*;
+//
+///**
+// * 통화 엔티티
+// * 통화 등록 시 사용되는 엔티티
+// */
+//@Entity
+//@Getter
+//@Builder
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@AllArgsConstructor(access = AccessLevel.PROTECTED)
+//public class Currency {
+//
+//    // 고유 식별자
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    // 통화 코드
+//    @Column(nullable = false)
+//    private String code;
+//
+//    // 통화명
+//    @Column(nullable = false)
+//    private String name;
+//
+//    // 환율
+//    @Column(nullable = false)
+//    private Double exchangeRate;
+//}
