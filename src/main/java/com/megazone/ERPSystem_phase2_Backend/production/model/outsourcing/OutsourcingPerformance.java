5a03da58702e15ec294e9d764ed57cb277dffdc5
wjdgus1016 <158249333+wjdgus1016@users.noreply.github.com>
2024-08-18 20:37:51 +0900

    feature: 외주, 자재, 설비 엔티티 추가 및 수정
    엔티티 추가 및 수정

--- src/main/java/com/megazone/ERPSystem_phase2_Backend/production/model/outsourcing/OutsourcingPerformance.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/production/model/outsourcing/OutsourcingPerformance.java
@@ -7,11 +7,15 @@ package com.megazone.ERPSystem_phase2_Backend.production.model.outsourcing;
     남품된 제품이 모두 제대로 도착했는지 확인.
 */
 //import com.megazone.ERPSystem_phase2_Backend.production.model.basic_information.Workcenter;
+import com.megazone.ERPSystem_phase2_Backend.production.model.basic_data.Workcenter;
+import com.megazone.ERPSystem_phase2_Backend.production.model.resource_data.MaterialData;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
@@ -25,19 +29,19 @@ public class OutsourcingPerformance {
     private Long id;                              //pk
 
     @Column(nullable = false)
-    private Date performanceDate;                   //실적일 (납품된 날짜)
+    private LocalDate performanceDate;                   //실적일 (납품된 날짜)
 
     @Column(nullable = false)
-    private String sortation;                       //구분
+    private String sortation;                       //구분   무슨구분?
 
     @Column(nullable = false)
-    private String performanceClassification;       //실적 구분 (적합/비적합)
+    private Boolean performanceClassification;       //실적 구분 (적합/비적합)
 
     @Column(nullable = false)
-    private Long performanceQuantity;               //실적 수량 (납품된 수량)
+    private BigDecimal performanceQuantity;               //실적 수량 (납품된 수량)
 
     @Column(nullable = false)
-    private String prosecutor;                      //검사(대기/합격/불합격)
+    private String prosecutor;                      //검사(대기/합격/불합격)    enum
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "outsourcingOrders_id")
@@ -46,16 +50,16 @@ public class OutsourcingPerformance {
     @OneToMany(mappedBy = "outsourcingPerformance", fetch = FetchType.LAZY)
     private List<OutsourcingInspections> outsourcingInspections;      //외주 실적 검사    하나의 남품 실적이 여러번의 검사 결과를 가질 수 있음 (재검사)
 
-//    자재 사용 유무
+    //자재 사용 유무
 //    @ManyToOne(fetch = FetchType.LAZY, optional = false)
 //    @JoinColumn(name = "meterialData_id")
-//    private String materialStatus;                //자재 현황
+//    private MaterialData materialStatus;                //자재 현황
 
 //    @ManyToOne(fetch = FetchType.LAZY , optional = false)
 //    @JoinColumn(name = "process_id")
 //    private ProcessDetails process;             //공정
 //
-//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-//    @JoinColumn(name = "workcenter_id")
-//    private Workcenter workcenter;       //작업장
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "workcenter_id")
+    private Workcenter workcenter;       //작업장
 }
