26f7d13d124ccdd0913cc2f290242df698ac22e7
hshhwa <hshdla@naver.com>
2024-08-19 12:10:48 +0900

    feature : 매출매입전표 enum type 추가

--- src/main/java/com/megazone/ERPSystem_phase2_Backend/financial/model/sales_and_purchase_voucher_entry/ResolvedSaleAndPurchaseVoucher.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/financial/model/sales_and_purchase_voucher_entry/ResolvedSaleAndPurchaseVoucher.java
@@ -1,4 +1,19 @@
 package com.megazone.ERPSystem_phase2_Backend.financial.model.sales_and_purchase_voucher_entry;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "resolved_sale_and_purchase_voucher")
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "resolved_sale_and_purchase_voucher")
 public class ResolvedSaleAndPurchaseVoucher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
--- src/main/java/com/megazone/ERPSystem_phase2_Backend/financial/model/sales_and_purchase_voucher_entry/enums/ElectronicTaxInvoiceStatus.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/financial/model/sales_and_purchase_voucher_entry/enums/ElectronicTaxInvoiceStatus.java
@@ -1,4 +1,26 @@
 package com.megazone.ERPSystem_phase2_Backend.financial.model.sales_and_purchase_voucher_entry.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.megazone.ERPSystem_phase2_Backend.financial.model.voucher_entry.general_voucher_entry.enums.VoucherType;
+
+import java.util.Arrays;
+
 public enum ElectronicTaxInvoiceStatus {
+    PUBLISHED("Published"),
+    UNPUBLISHED("Unpublished");
+
+
+    private final String name;
+
+    ElectronicTaxInvoiceStatus(String name) {
+        this.name = name;
+    }
+
+    @JsonCreator
+    public static ElectronicTaxInvoiceStatus of(String progress) {
+        return Arrays.stream(ElectronicTaxInvoiceStatus.values())
+                .filter(i -> i.name.equals(progress))
+                .findAny()
+                .orElse(null);
+    }
 }
