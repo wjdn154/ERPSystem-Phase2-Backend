c6571cd5d14feb5f322c7e71801f5e5e2b2241d2
Heeyoun Lim <heeyn.lim@gmail.com>
2024-08-19 11:49:32 +0900

    feature: Workcenter Controller 작성중

---
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/production/controller/basic_data/WorkcenterController.java
@@ -0,0 +1,29 @@
+//package com.megazone.ERPSystem_phase2_Backend.production.controller.basic_data;
+//
+//import com.megazone.ERPSystem_phase2_Backend.production.model.basic_data.Workcenter;
+//import com.megazone.ERPSystem_phase2_Backend.production.repository.basic_data.Workcenter.WorkcenterRepository;
+//import com.megazone.ERPSystem_phase2_Backend.production.service.basic_data.workcenter.WorkcenterRegistrationService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PutMapping;
+//
+//@Controller
+//@RequiredArgsConstructor
+//public class WorkcenterController {
+//
+//    private final WorkcenterRegistrationService workcenterRegistrationService;
+//    private final WorkcenterRepository workcenterRepository;
+//
+//    @GetMapping("/api/production/workcenters")
+//
+//    @PostMapping("api/production/workcenter/save/{code}")
+//
+//    @PutMapping("api/production/workcenter/save/{code}")
+//
+//    @DeleteMapping
+//
+//}
