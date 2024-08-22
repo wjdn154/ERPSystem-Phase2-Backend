fd08e9777593b258ebadfea099511bc1c093d435
ready-oun <heeyn.lim@gmail.com>
2024-08-22 16:19:09 +0900

    fix: Qclass 재생성 후 클래스로딩 이슈 해결 - RoutingStep까지 모든 routing management 리포지토리 및 서비스에서   빌드 에러가 났는데 안 잡혔던 것 -> 주석 처리하면서 빌드 성공하여 재생성된 파일이 인식됨 - 그전에도 빌드가 정상적으로 완료되었었는데 안 됐던 이유는 아직 모르겠음

--- src/main/java/com/megazone/ERPSystem_phase2_Backend/production/repository/routing_management/ProductionRouting/ProductionRoutingRepositoryImpl.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/production/repository/routing_management/ProductionRouting/ProductionRoutingRepositoryImpl.java
@@ -1,29 +1,29 @@
-//package com.megazone.ERPSystem_phase2_Backend.production.repository.routing_management.ProductionRouting;
-//
-//import com.megazone.ERPSystem_phase2_Backend.production.model.routing_management.ProductionRouting;
-//import com.megazone.ERPSystem_phase2_Backend.production.model.routing_management.QRoutingStep;
-//import com.megazone.ERPSystem_phase2_Backend.production.model.routing_management.;
-//import com.querydsl.jpa.impl.JPAQueryFactory;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.List;
-//
-//@Repository
-//@RequiredArgsConstructor
-//public class ProductionRoutingRepositoryImpl implements ProductionRoutingRepositoryCustom {
-//
-//    private final JPAQueryFactory queryFactory;
-//
-//    @Override
-//    public List<ProductionRouting> findRoutingsByProcessDetails(Long processId) {
-//        QProductionRouting productionRouting = QProductionRouting.productionRouting;
-//        QRoutingStep routingStep = QRoutingStep.routingStep;
-//
-//        return queryFactory.selectFrom(productionRouting)
-//                .join(productionRouting.routingSteps, routingStep)
-//                .where(routingStep.process.id.eq(processId))
-//                .fetch();
-//
-//    }
-//}
+package com.megazone.ERPSystem_phase2_Backend.production.repository.routing_management.ProductionRouting;
+
+import com.megazone.ERPSystem_phase2_Backend.production.model.routing_management.ProductionRouting;
+import com.megazone.ERPSystem_phase2_Backend.production.model.routing_management.QRoutingStep;
+import com.megazone.ERPSystem_phase2_Backend.production.model.routing_management.QProductionRouting;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ProductionRoutingRepositoryImpl implements ProductionRoutingRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<ProductionRouting> findRoutingsByProcessDetails(Long processId) {
+        QProductionRouting productionRouting = QProductionRouting.productionRouting;
+        QRoutingStep routingStep = QRoutingStep.routingStep;
+
+        return queryFactory.selectFrom(productionRouting)
+                .join(productionRouting.routingSteps, routingStep)
+                .where(routingStep.process.id.eq(processId))
+                .fetch();
+
+    }
+}
