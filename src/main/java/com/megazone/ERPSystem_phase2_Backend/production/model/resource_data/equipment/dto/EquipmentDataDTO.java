b67edc8d9a79f68b0573fff574cd9d9ab822f249
wjdgus1016 <158249333+wjdgus1016@users.noreply.github.com>
2024-08-23 02:17:40 +0900

    feature: 설비 crud 기능 구현
    설비 crud 기능 구현 ㅑ

--- src/main/java/com/megazone/ERPSystem_phase2_Backend/production/model/resource_data/equipment/dto/EquipmentDataDTO.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/production/model/resource_data/equipment/dto/EquipmentDataDTO.java
@@ -30,8 +30,8 @@ public class EquipmentDataDTO {
     private LocalDate installDate;              //설비 설치날짜
     private OperationStatus operationStatus;    //설비 상태 (가동중/유지보수중/고장/수리중)
     private BigDecimal cost;                    //설비 구매 비용
-    private Long workcenterId;                //설비가 설치된 위치 or 구역 (작업장). 작업장 테이블 작업장 코드 참조
-    private Long factoryId;               //설비가 설치된 공장 . 창고 테이블에 있는 공장코드 참조
+    private String workcenterCode;                //설비가 설치된 위치 or 구역 (작업장). 작업장 테이블 작업장 코드 참조
+    private String factoryCode;               //설비가 설치된 공장 . 창고 테이블에 있는 공장코드 참조
     private String equipmentImg;                //설비 이미지
 
 
