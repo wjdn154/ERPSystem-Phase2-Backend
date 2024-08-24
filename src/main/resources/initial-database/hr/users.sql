bb69617b0962eb53be9dac10cd8b840d7dedc367
Minseong <ckacl2512@naver.com>
2024-08-24 14:15:02 +0900

    feature: sql 수정
    sql 수정

--- src/main/resources/initial-database/hr/users.sql
+++ src/main/resources/initial-database/hr/users.sql
@@ -1,6 +1,9 @@
 INSERT INTO users_role (role_name,role_type) VALUES
-    ('대장','APPROVER');
+('대장','APPROVER');
 
+INSERT INTO users_permission(permission_name) VALUES
+('READ_PRIVILEGES'),
+('CONFIGURE_PRIVILEGES');
 
 INSERT INTO users (employee_id, role_id,password,user_name) VALUES
 (1,1,'12345678','만수이'),
