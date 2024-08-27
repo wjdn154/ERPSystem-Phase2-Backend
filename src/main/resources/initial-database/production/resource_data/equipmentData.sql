50a4f5b5e02715d76c9a6a72bb44b36c52c23f53
wjdgus1016 <158249333+wjdgus1016@users.noreply.github.com>
2024-08-27 20:38:46 +0900

    feature: 설비 초기데이터 추가
    초기데이터 추가

--- src/main/resources/initial-database/production/resource_data/equipmentData.sql
+++ src/main/resources/initial-database/production/resource_data/equipmentData.sql
@@ -1,7 +1,19 @@
 
 
 INSERT INTO equipment_data (cost,install_date,purchase_date,factory_id,workcenter_id,equipment_img,equipment_name,equipment_num,manufacturer,model_name,equipment_type,operation_status) VALUES
-    (1,'2022-02-02','2022-02-03',1,1,'설비 사진','조립설비1','PRD-EM1','삼성','MZ1','ASSEMBLY','BEFORE_OPERATION');
+    (100,'2022-02-02','2022-02-03',1,1,'설비 사진1','조립설비1','PRD-EM1','삼성','MZ1','ASSEMBLY','BEFORE_OPERATION');
 
 INSERT INTO equipment_data (cost,install_date,purchase_date,factory_id,workcenter_id,equipment_img,equipment_name,equipment_num,manufacturer,model_name,equipment_type,operation_status) VALUES
-    (1000,'2022-02-02','2022-02-04',1,1,'설비 사진2','제조설비2','PRD-EM2','삼성','MZ','ASSEMBLY','BEFORE_OPERATION');
+    (1000,'2022-02-02','2022-02-04',2,1,'설비 사진2','제조설비2','PRD-EM2','LG','MZ','ASSEMBLY','BEFORE_OPERATION');
+
+INSERT INTO equipment_data (cost,install_date,purchase_date,factory_id,workcenter_id,equipment_img,equipment_name,equipment_num,manufacturer,model_name,equipment_type,operation_status) VALUES
+    (1000,'2022-02-02','2022-02-04',1,1,'설비 사진3','제조설비2','PRD-EM3','YG','MZ','ASSEMBLY','BEFORE_OPERATION');
+
+INSERT INTO equipment_data (cost,install_date,purchase_date,factory_id,workcenter_id,equipment_img,equipment_name,equipment_num,manufacturer,model_name,equipment_type,operation_status) VALUES
+    (100000,'2022-02-02','2022-02-04',1,1,'설비 사진4','제조설비2','PRD-EM4','현대','MZ','ASSEMBLY','BEFORE_OPERATION');
+
+INSERT INTO equipment_data (cost,install_date,purchase_date,factory_id,workcenter_id,equipment_img,equipment_name,equipment_num,manufacturer,model_name,equipment_type,operation_status) VALUES
+    (10000,'2022-02-02','2022-02-04',2,1,'설비 사진5','제조설비2','PRD-EM5','삼성','MZ','ASSEMBLY','BEFORE_OPERATION');
+
+INSERT INTO equipment_data (cost,install_date,purchase_date,factory_id,workcenter_id,equipment_img,equipment_name,equipment_num,manufacturer,model_name,equipment_type,operation_status) VALUES
+    (111111,'2022-02-02','2022-02-04',1,1,'설비 사진6','제조설비2','PRD-EM6','삼성','MZ','ASSEMBLY','BEFORE_OPERATION');
