c44fdbf3e8f2cfd7ef35b731c834c6452c3e2922
ready-oun <heeyn.lim@gmail.com>
2024-09-05 11:58:02 +0900

    docs: hr employee 초기데이터 수정
    기존 데이터 주석처리 및 새 데이터 추가

--- src/main/resources/initial-database/hr/employee.sql
+++ src/main/resources/initial-database/hr/employee.sql
@@ -13,10 +13,16 @@ INSERT INTO employee_job_title (description, title_name) VALUES
 ('2','2'),
 ('3','3');
 
-INSERT INTO employee (date_of_birth, hire_date, is_household_head, department_id, job_title_id, position_id, address, email, employee_number, first_name, last_name, phone_number, profile_picture, employment_status, employment_type) VALUES
-('2000-07-20','2024-08-20',TRUE,1, 1,  1,'부산','ckacl2512@naver.com','0001','김', '민성','010-2512-9361','x','ACTIVE', 'INTERN'),
-('2000-07-20','2024-08-20',TRUE,2, 2,  2,'부산','ckacl2512@naver.com','0002','김', '민성','010-2512-9361','x','ACTIVE', 'INTERN'),
-('2000-07-20','2024-08-20',TRUE,3, 3,  3,'부산','ckacl2512@naver.com','0003','김', '민성','010-2512-9361','x','ACTIVE', 'INTERN');
+-- INSERT INTO employee (date_of_birth, hire_date, is_household_head, department_id, job_title_id, position_id, address, email, employee_number, first_name, last_name, phone_number, profile_picture, employment_status, employment_type) VALUES
+-- ('2000-07-20','2024-08-20',TRUE,1, 1,  1,'부산','ckacl2512@naver.com','0001','김', '민성','010-2512-9361','x','ACTIVE', 'INTERN'),
+-- ('2000-07-20','2024-08-20',TRUE,2, 2,  2,'부산','ckacl2512@naver.com','0002','김', '민성','010-2512-9361','x','ACTIVE', 'INTERN'),
+-- ('2000-07-20','2024-08-20',TRUE,3, 3,  3,'부산','ckacl2512@naver.com','0003','김', '민성','010-2512-9361','x','ACTIVE', 'INTERN');
+
+INSERT INTO employee (date_of_birth, hire_date, is_household_head, department_id, job_title_id, position_id, address, email, employee_number, first_name, last_name, phone_number, profile_picture, employment_status, employment_type)
+VALUES
+    ('1990-05-15', '2021-04-01', TRUE, 1, 1, 1, '서울', 'jiho.kim@example.com', '1001', '지호', '김', '010-1234-5678', 'img1.jpg', 'ACTIVE', 'FULL_TIME'),
+    ('1985-11-22', '2020-07-15', FALSE, 2, 2, 2, '부산', 'sungmin.lee@example.com', '1002', '성민', '이', '010-9876-5432', 'img2.jpg', 'ACTIVE', 'PART_TIME'),
+    ('1995-03-10', '2022-03-20', TRUE, 3, 3, 1, '대구', 'soojin.choi@example.com', '1003', '수진', '최', '010-2234-5678', 'img3.jpg', 'ACTIVE', 'INTERN'),
 
 INSERT INTO employee_bank_account (employee_id,account_number, bank_name) VALUES
 (1,'291-12-0239314','부산은행'),
