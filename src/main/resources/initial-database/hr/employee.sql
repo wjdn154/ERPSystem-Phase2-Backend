e965c3a40673d933a772148a8b4e3b3f0194894f
ready-oun <heeyn.lim@gmail.com>
2024-09-05 12:04:28 +0900

    refactor: hr employee 초기데이터 수정 & 실행확인

--- src/main/resources/initial-database/hr/employee.sql
+++ src/main/resources/initial-database/hr/employee.sql
@@ -20,9 +20,15 @@ INSERT INTO employee_job_title (description, title_name) VALUES
 
 INSERT INTO employee (date_of_birth, hire_date, is_household_head, department_id, job_title_id, position_id, address, email, employee_number, first_name, last_name, phone_number, profile_picture, employment_status, employment_type)
 VALUES
-    ('1990-05-15', '2021-04-01', TRUE, 1, 1, 1, '서울', 'jiho.kim@example.com', '1001', '지호', '김', '010-1234-5678', 'img1.jpg', 'ACTIVE', 'FULL_TIME'),
-    ('1985-11-22', '2020-07-15', FALSE, 2, 2, 2, '부산', 'sungmin.lee@example.com', '1002', '성민', '이', '010-9876-5432', 'img2.jpg', 'ACTIVE', 'PART_TIME'),
-    ('1995-03-10', '2022-03-20', TRUE, 3, 3, 1, '대구', 'soojin.choi@example.com', '1003', '수진', '최', '010-2234-5678', 'img3.jpg', 'ACTIVE', 'INTERN'),
+    ('1990-05-15', '2021-04-01', TRUE, 1, 1, 1, '서울', 'jiho.kim@example.com', '1001', '지호', '김', '010-1234-5678', 'img1.jpg', 'ACTIVE', 'FULL_TIME');
+
+INSERT INTO employee (date_of_birth, hire_date, is_household_head, department_id, job_title_id, position_id, address, email, employee_number, first_name, last_name, phone_number, profile_picture, employment_status, employment_type)
+VALUES
+    ('1985-11-22', '2020-07-15', FALSE, 2, 2, 2, '부산', 'sungmin.lee@example.com', '1002', '성민', '이', '010-9876-5432', 'img2.jpg', 'ACTIVE', 'PART_TIME');
+
+INSERT INTO employee (date_of_birth, hire_date, is_household_head, department_id, job_title_id, position_id, address, email, employee_number, first_name, last_name, phone_number, profile_picture, employment_status, employment_type)
+VALUES
+    ('1995-03-10', '2022-03-20', TRUE, 3, 3, 1, '대구', 'soojin.choi@example.com', '1003', '수진', '최', '010-2234-5678', 'img3.jpg', 'ACTIVE', 'INTERN');
 
 INSERT INTO employee_bank_account (employee_id,account_number, bank_name) VALUES
 (1,'291-12-0239314','부산은행'),
