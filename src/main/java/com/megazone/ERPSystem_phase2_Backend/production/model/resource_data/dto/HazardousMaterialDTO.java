dc61b22c60ef144d0d9f6c9d4661b646caeec416
wjdgus1016 <158249333+wjdgus1016@users.noreply.github.com>
2024-09-19 13:21:20 +0900

    feature: 유해물질 dto 추가
    유해물질 dto 추가

---
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/production/model/resource_data/dto/HazardousMaterialDTO.java
@@ -0,0 +1,18 @@
+package com.megazone.ERPSystem_phase2_Backend.production.model.resource_data.dto;
+
+import com.megazone.ERPSystem_phase2_Backend.production.model.resource_data.enums.HazardLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class HazardousMaterialDTO {
+
+    private Long id;
+    private String hazardousMaterialCode;    //유해물질 코드
+    private String hazardousMaterialName;    //유해물질 이름
+    private HazardLevel hazardLevel;         //위험등급
+    private String description;              //설명
+}
--- src/main/java/com/megazone/ERPSystem_phase2_Backend/production/model/resource_data/dto/ListMaterialDataDTO.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/production/model/resource_data/dto/ListMaterialDataDTO.java
@@ -1,7 +1,24 @@
 package com.megazone.ERPSystem_phase2_Backend.production.model.resource_data.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ListMaterialDataDTO {
 
+    private Long id;
+    private String materialCode;           //자재 코드
+    private String materialName;           //자재 이름
+    private String materialType;           //자재유형
+    private Long stockQuantity;            //재고 수량
+    private BigDecimal purchasePrice;      //구매 가격
     private String representativeName;     //거래처 명 (한 거래처에 여러개의 자재)
-    private String productName;             //품목 이름(한 품목에 여러개의 자재)
+    private String productName;             //품목 이름(한 품목에 여러개의 자재? 한 자재에 여러개의 품목?)
+    private List<HazardousMaterialDTO> hazardousMaterial;   //유해물질 리스트
 }
