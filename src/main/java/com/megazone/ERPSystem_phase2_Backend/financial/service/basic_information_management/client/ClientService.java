cbf080312b93685cf13c0ad60a2733dd70aac6e1
허건호 <gj1563@naver.com>
2024-10-01 21:18:30 +0900

    feature: 거래처 관련 기능 추가

--- src/main/java/com/megazone/ERPSystem_phase2_Backend/financial/service/basic_information_management/client/ClientService.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/financial/service/basic_information_management/client/ClientService.java
@@ -4,12 +4,17 @@ import com.megazone.ERPSystem_phase2_Backend.financial.model.basic_information_m
 import com.megazone.ERPSystem_phase2_Backend.financial.model.basic_information_management.account_subject.dto.AccountSubjectsAndMemosDTO;
 import com.megazone.ERPSystem_phase2_Backend.financial.model.basic_information_management.client.dto.ClientDTO;
 import com.megazone.ERPSystem_phase2_Backend.financial.model.basic_information_management.company.dto.CompanyDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 public interface ClientService {
     Optional<ClientDTO> saveClient(ClientDTO clientDTO);
     Optional<ClientDTO> updateClient(Long id, ClientDTO clientDTO);
     List<ClientDTO> searchClient(String searchText);
+    ResponseEntity<Object> fetchClientList();
+    ResponseEntity<Object> fetchClient(Long id);
 }
