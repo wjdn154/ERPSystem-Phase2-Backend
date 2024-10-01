41cda98f98558e7043794b197ab39c9fb4c7e7bc
wjdgus1016 <158249333+wjdgus1016@users.noreply.github.com>
2024-10-02 02:48:43 +0900

    작업실적 crud 로직 생성, 품질검사 controller 작성,dto 수정

--- src/main/java/com/megazone/ERPSystem_phase2_Backend/production/model/work_performance/quality_control/dto/InboundRegistrationDetailDTO.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/production/model/work_performance/quality_control/dto/InboundRegistrationDetailDTO.java
@@ -5,9 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+
 public class InboundRegistrationDetailDTO {
 }
