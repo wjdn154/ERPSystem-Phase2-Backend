de53e071d8468783c86794346c717efca4b1e00b
허건호 <gj1563@naver.com>
2024-10-28 13:53:51 +0900

    feature: 중간저장

--- src/main/java/com/megazone/ERPSystem_phase2_Backend/financial/service/voucher_entry/sales_and_purchase_voucher_entry/UnresolvedSaleAndPurchaseVoucherServiceImpl.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/financial/service/voucher_entry/sales_and_purchase_voucher_entry/UnresolvedSaleAndPurchaseVoucherServiceImpl.java
@@ -402,11 +402,19 @@ public class UnresolvedSaleAndPurchaseVoucherServiceImpl implements UnresolvedSa
                 throw new IllegalArgumentException("권한 또는 해당하는 전표가 없습니다.");
             }
 
-            recentActivityRepository.save(RecentActivity.builder()
-                    .activityDescription("미결 매출매입전표 " + unresolvedVoucherList.size() + "건 승인")
-                    .activityType(ActivityType.FINANCE)
-                    .activityTime(LocalDateTime.now())
-                    .build());
+            if (dto.getApprovalStatus().equals(ApprovalStatus.APPROVED)){
+                recentActivityRepository.save(RecentActivity.builder()
+                        .activityDescription("미결 매출매입전표 " + unresolvedVoucherList.size() + "건 승인")
+                        .activityType(ActivityType.FINANCE)
+                        .activityTime(LocalDateTime.now())
+                        .build());
+            }else if (dto.getApprovalStatus().equals(ApprovalStatus.REJECTED)){
+                recentActivityRepository.save(RecentActivity.builder()
+                        .activityDescription("미결 매출매입전표 " + unresolvedVoucherList.size() + "건 반려")
+                        .activityType(ActivityType.FINANCE)
+                        .activityTime(LocalDateTime.now())
+                        .build());
+            }
 
         return unresolvedVoucherList;
     }
