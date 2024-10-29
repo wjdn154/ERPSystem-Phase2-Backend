62c5f6f400f8a3f940e3d0e0e8c2966961f57ddd
ready-oun <heeyn.lim@gmail.com>
2024-10-29 13:11:21 +0900

    fix: 롤백

--- src/main/java/com/megazone/ERPSystem_phase2_Backend/production/repository/production_schedule/common_scheduling/production_order/ProductionOrderRepository.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/production/repository/production_schedule/common_scheduling/production_order/ProductionOrderRepository.java
@@ -1,5 +1,6 @@
 package com.megazone.ERPSystem_phase2_Backend.production.repository.production_schedule.common_scheduling.production_order;
 
+import com.megazone.ERPSystem_phase2_Backend.production.model.basic_data.workcenter.Workcenter;
 import com.megazone.ERPSystem_phase2_Backend.production.model.production_schedule.common_scheduling.ProductionOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ProductionOrderRepository extends JpaRepository<ProductionOrder, Long>, ProductionOrderRepositoryCustom {
-    Optional<ProductionOrder> findByName(String productionOrderName);
+
     List<ProductionOrder> findByMpsId(Long mpsId);
 
 
--- src/main/java/com/megazone/ERPSystem_phase2_Backend/production/repository/production_schedule/common_scheduling/production_order/ProductionOrderRepositoryCustom.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/production/repository/production_schedule/common_scheduling/production_order/ProductionOrderRepositoryCustom.java
@@ -1,11 +1,6 @@
 package com.megazone.ERPSystem_phase2_Backend.production.repository.production_schedule.common_scheduling.production_order;
 
-import java.time.LocalDate;
 
 public interface ProductionOrderRepositoryCustom {
-
-//    boolean existsByWorkerIdAndAssignmentDateAndDifferentWorkcenter(Long workerId, LocalDate assignmentDate, String workcenterCode);
-//
-//    boolean existsByWorkerIdAndAssignmentDateAndDifferentShiftType(Long workerId, LocalDate assignmentDate, Long shiftTypeId);
 
 }
