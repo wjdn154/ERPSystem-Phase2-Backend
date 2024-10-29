5db8921f6479a9579009687394fd522f33509314
허건호 <gj1563@naver.com>
2024-10-29 09:41:58 +0900

    fix: gradle 오류 수정

--- src/main/java/com/megazone/ERPSystem_phase2_Backend/production/repository/production_schedule/common_scheduling/production_order/ProductionOrderRepository.java
+++ src/main/java/com/megazone/ERPSystem_phase2_Backend/production/repository/production_schedule/common_scheduling/production_order/ProductionOrderRepository.java
@@ -1,5 +1,6 @@
 package com.megazone.ERPSystem_phase2_Backend.production.repository.production_schedule.common_scheduling.production_order;
 
+import com.megazone.ERPSystem_phase2_Backend.production.model.basic_data.workcenter.Workcenter;
 import com.megazone.ERPSystem_phase2_Backend.production.model.production_schedule.common_scheduling.ProductionOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
